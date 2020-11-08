80cc692f0e672507a8c1e825931d3a158f129d71
David Jacquet <jacquet.david@gmail.com>
2020-11-08 23:39:23 +0100

    Fetching df from polygon, nice! ...

--- polygon/rest/models/pm.py
+++ polygon/rest/models/pm.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 import keyword
 import typing
+import datetime
 from typing import List, Dict, Any
 
 from polygon import RESTClient
@@ -8,6 +9,8 @@ from polygon.rest import models
 from pydantic import BaseModel
 from pydantic import BaseModel, Field
 import requests
+import functools
+import pandas as pd
 
 _T = typing.TypeVar('_T')
 
@@ -17,12 +20,13 @@ class PolygonModel(BaseModel):
         client: RESTClient = None
 
     @classmethod
-    def _get(cls: PolygonModel, path: str, **kwargs) -> PolygonModel:
+    def _get(cls: PolygonModel, path: str, params:dict=None) -> PolygonModel:
         c = PolygonModel.Meta.client
         assert c is not None
-        r = requests.Response = c._session.get(f"{c.url}{path}", **kwargs)
+        r = requests.Response = c._session.get(f"{c.url}{path}", params=params)
         if r.status_code == 200:
-            d : dict = r.json()
+            d: dict = r.json()
+            print(d)
             # noinspection PyArgumentList
             return cls(**d)
         else:
@@ -34,6 +38,7 @@ class PolygonModel(BaseModel):
 
 
 StockSymbol = str
+
 
 class TickerDetail(PolygonModel):
     logo: str
@@ -60,5 +65,34 @@ class TickerDetail(PolygonModel):
     updated: str
 
     @classmethod
+    @functools.lru_cache()
     def get(cls, symbol: str, **kwargs) -> TickerDetail:
         return TickerDetail._get(f"/v1/meta/symbols/{symbol}/company")
+
+
+class Bar(BaseModel):
+    volume: int = Field(alias='v')
+    open: float = Field(alias='o')
+    close: float = Field(alias='c')
+    high: float = Field(alias='h')
+    low: float = Field(alias='l')
+    utc_window_start: datetime.datetime = Field(alias='t')
+    window_size: int = Field(alias='n')
+
+
+class TickerWindow(PolygonModel):
+    ticker: StockSymbol
+    status: str
+    adjusted: bool
+    query_count: int = Field('queryCount')
+    results: typing.List[Bar]
+
+    @classmethod
+    def get(cls: TickerWindow, ticker: StockSymbol, timespan: str, from_: str, to: str, multiplier: int = 1,
+            unadjusted: bool = False, sort: str = 'asc') -> TickerWindow:
+        return TickerWindow._get(f"/v2/aggs/ticker/{ticker}/range/{multiplier}/{timespan}/{from_}/{to}",
+                                 params=dict(sort=sort, unadjusted=unadjusted))
+
+    @property
+    def df(self) -> pd.DataFrame:
+        return pd.DataFrame.from_dict(self.dict()['results'])
