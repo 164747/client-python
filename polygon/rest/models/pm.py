cf4f15ce90f470666ec7fa16ac1f14d715bc617e
David Jacquet <jacquet.david@gmail.com>
2020-11-21 01:06:26 +0100

    Pair trader on the way ...

--- polygon/rest/models/pm.py
+++ polygon/rest/models/pm.py
@@ -1,19 +1,18 @@
 from __future__ import annotations
-import keyword
-import typing
+
 import datetime
-from typing import List, Dict, Any
+import functools
+import typing
+from typing import List
+
+import pandas as pd
+import requests
+from pydantic import BaseModel, Field
 
 from polygon import RESTClient
-from polygon.rest import models
-from pydantic import BaseModel
-from pydantic import BaseModel, Field
-import requests
-import functools
-import pandas as pd
-from dateutil.parser import parser
 
-_T = typing.TypeVar('_T')
+# _T = typing.TypeVar('_T')
+_T = typing.ClassVar
 
 
 class PolygonModel(BaseModel):
@@ -21,23 +20,62 @@ class PolygonModel(BaseModel):
         client: RESTClient = None
 
     @classmethod
-    def _get(cls: PolygonModel, path: str, params: dict = None) -> PolygonModel:
+    def _get(cls: _T, path: str, params: dict = None) -> typing.Union[_T, typing.List[_T]]:
         c = PolygonModel.Meta.client
         assert c is not None
         r = requests.Response = c._session.get(f"{c.url}{path}", params=params)
         if r.status_code == 200:
-            d: dict = r.json()
-            # noinspection PyArgumentList
-            return cls(**d)
+            d: typing.Union[dict, list] = r.json()
+            if isinstance(d, list):
+                # noinspection PyArgumentList
+                return [cls(**el) for el in d]
+            else:
+                # noinspection PyArgumentList
+                return cls(**d)
+
         else:
             r.raise_for_status()
 
     @classmethod
-    def get(cls: PolygonModel, *args, **kwargs) -> PolygonModel:
+    def get(cls: _T, *args, **kwargs) -> typing.Union[_T, typing.List[_T]]:
         raise NotImplementedError
 
 
 StockSymbol = str
+
+
+class Ticker(PolygonModel):
+    symbol: str = Field(alias='ticker')
+    name: str
+    market: str
+    locale: str
+    currency: str
+    active: bool
+    primary_exchange: str = Field(alias='primaryExch')
+    type_: str = Field(alias='type', default=None)
+    codes: typing.Dict[str, str] = None
+    updated: typing.Union[datetime.datetime, datetime.date]
+    url: str
+    attrs: typing.Dict[str, str] = None
+
+    @classmethod
+    def get(cls: _T, *args, **kwargs) -> typing.Union[_T, typing.List[_T]]:
+        raise NotImplementedError
+
+
+class TickerList(PolygonModel):
+    page: int
+    per_page: int = Field(alias='perPage')
+    count: int
+    status: str
+    tickers: typing.List[Ticker]
+
+    @classmethod
+    @functools.lru_cache()
+    def get(cls, market: str, search: str = None, active: str = 'true') -> TickerList:
+        params = locals()
+        params.pop('cls')
+        return TickerList._get(f"/v2/reference/tickers", params=params)
 
 
 class TickerDetail(PolygonModel):
@@ -49,7 +87,7 @@ class TickerDetail(PolygonModel):
     cik: str
     bloomberg: str
     figi: str = None
-    lei: str
+    lei: str = None
     sic: float
     country: str
     industry: str
@@ -77,15 +115,16 @@ class Bar(BaseModel):
     high: float = Field(alias='h')
     low: float = Field(alias='l')
     utc_window_start: datetime.datetime = Field(alias='t')
-    trades: int = Field(alias='n')
+    trades: int = Field(alias='n', default=0)
 
 
 class TickerWindow(PolygonModel):
-    ticker: StockSymbol
+    symbol: StockSymbol = Field(alias='ticker')
     status: str
     adjusted: bool
     query_count: int = Field('queryCount')
     results: typing.List[Bar]
+    _df : pd.DataFrame = Field(default_factory=pd.DataFrame)
 
     class __WindowSplitter(BaseModel):
         from_: datetime.date
@@ -103,25 +142,67 @@ class TickerWindow(PolygonModel):
             return res
 
     @classmethod
-    def get(cls: TickerWindow, ticker: StockSymbol, timespan: str, from_: str, to: str, multiplier: int = 1,
+    def get(cls: TickerWindow, symbol: StockSymbol, timespan: str, from_: str, to: str, multiplier: int = 1,
             unadjusted: bool = False, sort: str = 'asc') -> TickerWindow:
         # noinspection PyTypeChecker
         ws = TickerWindow.__WindowSplitter(**dict(from_=from_, to=to, timespan=timespan))
         res = None
         for (from_, to) in ws.split_list:
-            tmp = cls._get(f"/v2/aggs/ticker/{ticker}/range/{multiplier}/{timespan}/{from_}/{to}",
+            tmp = cls._get(f"/v2/aggs/ticker/{symbol}/range/{multiplier}/{timespan}/{from_}/{to}",
                            params=dict(sort=sort, unadjusted=unadjusted))
             if isinstance(res, TickerWindow):
-                res.append(tmp)
+                res.consume(tmp)
             else:
                 res = tmp
         return res
 
-    def append(self, other: TickerWindow):
-        self.query_count += other.query_count
-        self.results.extend(other.results)
+    def consume(self, other: TickerWindow):
+        d_orig = {bar.utc_window_start: bar for bar in self.results}
+        d_new = {bar.utc_window_start: bar for bar in other.results}
+        d_orig.update(d_new)
+        self.results = list(d_orig.values())
+        self.results.sort(key=lambda x: x.utc_window_start)
+        self.query_count = len(self.results)
+        _df : pd.DataFrame = Field(default_factory=pd.DataFrame)
 
     @property
     def df(self) -> pd.DataFrame:
-        df = pd.DataFrame.from_dict(self.dict()['results'])
-        return df.set_index('utc_window_start').sort_index()
+        if len(self._df) == 0:
+            df = pd.DataFrame.from_dict(self.dict()['results'])
+            self._df = df.set_index('utc_window_start').sort_index()
+        return self._df
+
+    def add_bar(self, bar : Bar):
+        if len(self.results) == 0 or bar.utc_window_start > self.results[-1].utc_window_start:
+            self.results.append(bar)
+            self._df = pd.DataFrame()
+        elif bar.utc_window_start == self.results[-1].utc_window_start:
+            self.results[-1] = bar
+        else:
+            raise NotImplementedError
+
+class TickerWindowFetcher(BaseModel):
+    max_date: datetime.date = Field(default_factory=datetime.date.today)
+    days_back: int = 5
+    timespan: str = 'minute'
+    symbol: StockSymbol
+    adjusted: bool = True
+
+    def get_ticker_window(self, new_start_date: bool = True) -> TickerWindow:
+        if new_start_date:
+            self.max_date = datetime.date.today()
+        max_date = self.max_date
+        min_date = max_date - datetime.timedelta(days=self.days_back)
+        res = None
+        tmp = max_date
+        while min_date < tmp:
+            tmp = max(min_date, max_date - datetime.timedelta(days=5))
+            tw = TickerWindow.get(self.symbol, self.timespan, tmp.isoformat(), max_date.isoformat(),
+                                  unadjusted=(not self.adjusted))
+            max_date = tmp
+            if res is None:
+                res = tw
+            else:
+                res.consume(tw)
+            print(min_date, max_date, tmp)
+        return res
---
+++ polygon/websocket/pm.py
@@ -0,0 +1,68 @@
+import time
+import typing
+import datetime
+import pytz
+from polygon.rest.models import pm
+from pydantic import BaseModel, Field
+
+
+class SocketBase(BaseModel):
+    symbol: str = Field(alias='sym')
+    event_type: str = Field(alias='ev')
+
+
+
+class Trade(SocketBase):
+    exchange_id: int = Field(alias='x')
+    trade_id: int = Field(alias='i')
+    tape: int = Field(alias='z')
+    price: float = Field(alias='p')
+    size: int = Field(alias='s')
+    trade_conditions: typing.List[int] = Field(alias='c', default=None)
+    utc: datetime.datetime = Field(alias='t')
+
+    @property
+    def age(self) -> datetime.timedelta:
+        return datetime.datetime.now(pytz.utc) - self.utc
+
+
+class Quote(SocketBase):
+    bid_exchange_id: int = Field(alias='bx')
+    bid_price: float = Field(alias='bp')
+    bid_size: int = Field(alias='bs')
+    ask_exchange_id: int = Field(alias='ax')
+    ask_price: float = Field(alias='ap')
+    ask_size: int = Field(alias='as')
+    quote_conditions: int = Field(alias='c')
+    utc: datetime.datetime = Field(alias='t')
+
+    @property
+    def age(self) -> datetime.timedelta:
+        return datetime.datetime.now(pytz.utc) - self.utc
+
+    @property
+    def payback(self) -> float:
+        return (self.bid_price / self.ask_price) ** .5
+
+    @property
+    def middle_price(self) -> float:
+        return (self.ask_price + self.bid_price) / 2.0
+
+
+
+class Bar(SocketBase):
+    volume : int = Field(alias='v')
+    volume_today : int = Field(alias='av')
+    official_open_price : float = Field(alias='op')
+    vol_weight_price : float = Field(alias='vw')
+    open_price : float = Field(alias='o')
+    close_price : float = Field(alias='c')
+    high_price : float = Field(alias='h')
+    low_price : float = Field(alias='l')
+    avg_prive : float = Field(alias='a')
+    utc_start : datetime.datetime = Field('s')
+    utc_end : datetime.datetime = Field('e')
+
+    @property
+    def rest_bar(self) -> pm.Bar:
+        return pm.Bar(v=self.volume, o=self.open_price, c=self.close_price, h=self.high_price, l=self.low_price, t=self.utc_start, n=1)
